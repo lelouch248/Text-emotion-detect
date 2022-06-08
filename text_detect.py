4e99f09b717b806c526493304576fdca3906556f
Karthik G <68554363+lelouch248@users.noreply.github.com>
2022-06-08 19:06:01 +0530

    Create text_detect.py

---
+++ text_detect.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+import joblib
+model = joblib.load('text-emotion-detect')
+st.title('User emotion detection application backend [ working implementation]')
+ip = st.text_input("Users message will be put here to predict the users emotion")
+op = model.predict(ip)
+if st.button('Analyse-emotion')
+  st.title(op[0])
