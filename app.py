d3ec6924e489cc6f3f79da142d3232b2afd2c76b
Karthik G <68554363+lelouch248@users.noreply.github.com>
2022-06-08 21:28:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+import joblib
+model = joblib.load('/content/model')
+st.title('User emotion detection application backend [ working implementation]')
+ip = st.text_input("Users message")
+op = model.predict([ip])
+if st.button("Analyse"):
+  st.title(op[0])
