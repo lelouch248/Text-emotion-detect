4291a0b949e9c3bcc129d4f9f2b9d822c392f91f
Karthik G <68554363+lelouch248@users.noreply.github.com>
2022-06-08 21:28:53 +0530

    Delete text_detect.py

--- text_detect.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st 
-import joblib
-model = joblib.load('model (1)')
-st.title('User emotion detection application backend [ working implementation]')
-ip = st.text_input("Users message")
-op = model.predict([ip])
-if st.button("Analyse"):
-  st.title(op[0])
-  
