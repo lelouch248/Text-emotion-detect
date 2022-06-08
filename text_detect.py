f6498d58e19095fd91bf784e612582438574dff6
Karthik G <68554363+lelouch248@users.noreply.github.com>
2022-06-08 21:02:09 +0530

    Update text_detect.py

--- text_detect.py
+++ text_detect.py
@@ -1,11 +1,9 @@
 import streamlit as st 
 import joblib
-model = joblib.load('model')
+model = joblib.load('/content/model')
 st.title('User emotion detection application backend [ working implementation]')
-ip = st.text_input("Users message will be put here to predict the users emotion")
-op = model.predict(ip)
-if st.button('Analyse-emotion')
+ip = st.text_input("Users message")
+op = model.predict([ip])
+if st.button("Analyse"):
   st.title(op[0])
-
-  
   
